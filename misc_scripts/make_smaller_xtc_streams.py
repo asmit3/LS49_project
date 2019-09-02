862d9459d5b35656eb8fb7d8cf93710409cd6f40
Asmit Bhowmick <asmit3@gmail.com>
2019-09-02 00:15:41 -0700

    only write out events from s00 because of mismatch with s80 streams for later events; this script now successfully writes out a short xtc stream with 4 events along with the fee data. XTC processing works on the xtc streams i.e able to read the wavelength from fee and index images

--- misc_scripts/make_smaller_xtc_streams.py
+++ misc_scripts/make_smaller_xtc_streams.py
@@ -29,6 +29,7 @@ class Dgram:
   def tslow(self): return self._header[2]&0xffffff
   def transitionId(self): return (self._header[2]>>24)&0x1f
   def tshigh(self): return self._header[3]
+  def fiducials(self): return self.tshigh()&0x1ffff
   def env(self): return self._header[4]
   def dmg(self): return self._header[5]
   def srclog(self): return self._header[6]
@@ -42,50 +43,104 @@ class Dgram:
       self._payload.tofile(outfile)
 
 
+def should_write_out_fee_data(seconds, fiducials, event_info):
+  ''' A function that evaluates if the seconds, fiducials are close enough 
+      to one of the entries in event_info
+      Close enough criterion:
+      a. difference in seconds < 5
+      b. fiducials should be equal
+      Function should return a bool '''
+   
+  for iid, evt in enumerate(event_info):
+    if evt[1] !=fiducials:
+      continue
+    if abs(evt[0]-seconds) >= 5:
+      continue
+    return True
+  return False
+
 if __name__=='__main__':
   '''
   Note these are cctbx timestamps. Look at corresponding high/low timestamps below
   that are read by psana
   timestamps_indexe=[
-  '20180501143548650',
-  '20180501143551715',
   '20180501143555114',
   '20180501143602713',
-  '20180501143625171',
   '20180501143628702',
-  '20180501143632300',
-  '20180501143643662',
-  '20180501143701853']
+  '20180501143632300
+  ]
   '''
   # Timestamps indexed by FFT1D using FEE wavelength for run 222
   timestamps_indexed = [
-    (1525185348, 650044434),
-    (1525185351, 715818581),
     (1525185355, 114751564),
     (1525185362, 713026761),
-    (1525185385, 171252956),
     (1525185388, 702989123),
-    (1525185392, 300990237),
-    (1525185403, 662398118),
-    (1525185421, 853941994)
+    (1525185392, 300990237)
   ]
 
-  outfile = open('junk.xtc','w')
-  for ii in range(4):
-    infile = open('/reg/d/psdm/mfx/mfxls4916/xtc/e1182-r0222-s0%d-c00.xtc'%ii,'r')
-    counter = 0
+  write_xrd_data=True
+  write_fee_data=False
+  if write_xrd_data:
+    print ('Writing out XRD data')
+    outfile = open('junk.xtc','w')
+    for ii in range(4):
+      infile = open('/reg/d/psdm/mfx/mfxls4916/xtc/e1182-r0222-s0%d-c00.xtc'%ii,'r')
+      counter = 0
+      while True:
+        try:
+          dg = Dgram(infile)
+          counter +=1
+          if dg.transitionId() !=12:
+            print ('DG stuff:: not transitionId 12  = ',dg.transitionId(),dg.clocklow(),dg.clockhigh())
+            dg.write(outfile)
+          if (dg.clockhigh(),dg.clocklow()) not in timestamps_indexed: continue
+          print ('DG stuff = ',dg.transitionId(),dg.clocklow(),dg.clockhigh(), dg.fiducials())
+          dg.write(outfile)
+        except Exception as e:
+          print ('Stream %d has %d frames = '%(ii, counter))
+          break
+      infile.close()
+    outfile.close()
+
+
+  # Now do the same with the s80 streams for the fee spec data
+  if write_fee_data:
+    print ('Writing out FEE data')
+    print ('First read in junk.xtc to read in the seconds,fiducials to be printed out')
+    infile=open('junk.xtc')
+    event_info = []
     while True:
       try:
-        dg = Dgram(infile)
-        counter +=1
-        if dg.transitionId() !=12:
-          print ('DG stuff:: not transitionId 12  = ',dg.transitionId(),dg.clocklow(),dg.clockhigh())
-          dg.write(outfile)
-        if (dg.clockhigh(),dg.clocklow()) not in timestamps_indexed: continue
-        print ('DG stuff = ',dg.transitionId(),dg.clocklow(),dg.clockhigh())
-        dg.write(outfile)
+        dg=Dgram(infile)
+        if dg.transitionId() !=12: continue 
+        seconds=dg.clockhigh()
+        fiducials=dg.fiducials()
+        event_info.append((seconds, fiducials))
       except Exception as e:
-        print ('Stream %d has %d frames = '%(ii, counter))
+        #from IPython import embed; embed(); exit()
+        infile.close()
         break
-    infile.close()
-  outfile.close()
+
+    print ('Now read in the s80 stream and see which events should be written out')
+    outfile = open('fee.xtc','w')
+    for ii in range(1):
+      infile = open('/reg/d/psdm/mfx/mfxls4916/xtc/e1182-r0222-s8%d-c00.xtc'%ii,'r')
+      counter = 0
+      while True:
+        try:
+          dg = Dgram(infile)
+          counter +=1
+          if dg.transitionId() !=12:
+            print ('DG stuff:: not transitionId 12  = ',dg.transitionId(),dg.clocklow(),dg.clockhigh())
+            dg.write(outfile)
+            continue
+          seconds=dg.clockhigh()
+          fiducials=dg.fiducials()
+          if not should_write_out_fee_data(seconds, fiducials, event_info): continue
+          print ('DG stuff = ',dg.transitionId(),dg.clocklow(),dg.clockhigh(), dg.fiducials())
+          dg.write(outfile)
+        except Exception as e:
+          print ('Stream %d has %d frames = '%(ii, counter))
+          break
+      infile.close()
+    outfile.close()
