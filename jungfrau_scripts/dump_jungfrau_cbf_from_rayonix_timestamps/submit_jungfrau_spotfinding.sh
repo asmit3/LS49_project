41682418a41d81d5e4de4c3673e936a28556fed2
Asmit <asmit3@gmail.com>
2020-01-15 17:52:38 -0800

    scripts changed now to be used on Cori to dump jungfrau CBFs

--- jungfrau_scripts/dump_jungfrau_cbf_from_rayonix_timestamps/submit_jungfrau_spotfinding.sh
+++ jungfrau_scripts/dump_jungfrau_cbf_from_rayonix_timestamps/submit_jungfrau_spotfinding.sh
@@ -1,3 +1,8 @@
+
+source ~/dials/dials_env.sh
+export SIT_PSDM_DATA=/global/cscratch1/sd/asmit/LS49/LS49_SAD_v3
+export SIT_DATA=/global/cscratch1/sd/asmit/LS10/psdm/psdm/data
+
 RUN=$1
 TRIAL=$2
 RG=$3
@@ -8,10 +13,10 @@ PHOTON_ENERGY=$6
 RUN_DIR="r$(printf "%04d" ${RUN})"
 TRIAL_RG="$(printf "%03d" ${TRIAL})_rg$(printf "%03d" ${RG})"
 RG_3d="rg_$(printf "%03d" ${RG})"
+
 OUT_DIR=${BASE_PATH}/${RUN_DIR}/${TRIAL_RG}/out
 
-##cctbx_args="$cctbx_args iota.timeout_cutoff_sec=2400"
-cctbx_args="spotfinder.lookup.mask=${BASE_PATH}/${RUN_DIR}/${TRIAL_RG}/mask.pickle output.output_dir=${OUT_DIR} output.logging_dir=${OUT_DIR}"
+cctbx_args="spotfinder.lookup.mask=${BASE_PATH}/${RUN_DIR}/${TRIAL_RG}/mask.pickle output.output_dir=${OUT_DIR} output.logging_dir=${OUT_DIR} LS49.path_to_rayonix_crystal_models=${BASE_PATH}/${RUN_DIR}/out_shoebox"
 echo $cctbx_args
 t_start=`date +%s`
-mpirun libtbx.python /reg/neh/home5/asmit/ExaFEL/dials/modules/exafel_project/ADSE13_25/spot_finding/find_spots.py ${BASE_PATH}/${RUN_DIR}/${TRIAL_RG}/data.loc ${BASE_PATH}/${RUN_DIR}/${TRIAL_RG}/params_1.phil ${cctbx_args}
+srun -n 16 -c 4 libtbx.python /global/u2/a/asmit/dials/modules/exafel_project/ADSE13_25/spot_finding/find_spots.py ${BASE_PATH}/${RUN_DIR}/${TRIAL_RG}/data.loc ${BASE_PATH}/${RUN_DIR}/${TRIAL_RG}/params_1.phil ${cctbx_args}
