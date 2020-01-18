8b02dfce74c905e049e2ae4b38c59bf1d1563a28
Asmit <asmit3@gmail.com>
2020-01-18 14:27:12 -0800

    second round of edits to mpi_load_ls49; WIP

--- diffBragg_work/mpi_load_ls49.py
+++ diffBragg_work/mpi_load_ls49.py
@@ -1,20 +1,71 @@
 #
 # MPI version of load_ls49
 #
-
+from __future__ import absolute_import, print_function, division 
+from libtbx.utils import Abort, Sorry
+from libtbx.phil import parse
 
 from LS49_project.diffBragg_work.load_ls49 import strong_spot_mask, process_ls49_image_real
 
 
+LS49_diffBragg_phil_str = ''' 
+LS49_diffBragg {
+  timestamps_file = None
+    .type = str
+    .help = Path to filename that contains all timestamps to be processed \
+  rayonix_expt_path = None
+    .type = str
+    .help = Path to folder that contains all rayonix expt/refl files
+  output_dir = None
+    .type = str
+    .help = Path to output directory 
+  plot = False
+    .type = bool
+    .help = Flag to indicate if plotting will be used.
+
+}
+
+'''
+phil_scope = parse(LS49_diffBragg_phil_str, process_includes=True)
+
 class Script(object):
   def __init__(self):
-    """initializer """
+    """Initialise the script."""
+    from dials.util.options import OptionParser
+    import libtbx.load_env
+    # Create the parser
+    self.parser = OptionParser(usage=usage, phil=phil_scope, epilog=help_message)
 
-  def run():
-    
+  def run(self):
+    """Execute the script."""
+    from dials.util import log
+    from time import time
+    from libtbx import easy_mp
+    import copy
 
-if __name__ == "__main__":
-    from argparse import ArgumentParser
+    # Parse the command line
+    params, options, all_paths = self.parser.parse_args(
+        show_diff_phil=False, return_unhandled=True, quick_parse=True)
+
+    # Save the options
+    self.options = options
+    self.params = params
+
+    from mpi4py import MPI
+    comm = MPI.COMM_WORLD
+    rank = comm.Get_rank()
+    size = comm.Get_size()
+    # Let rank 0 read in the timestamps to process and distribute the work
+    if rank == 0:
+      all_timestamps = []
+      with fin as open(self.params.LS49_diffBragg.timestamps_file, 'r'):
+        for line in fin:
+          if line !='/n':
+            all_timestamps.append(line.strip())
+    comm.Barrier()
+     
+
+  def do_work(self, item_list=None)
     from simtbx.diffBragg.refiners import RefineAll
     from simtbx.diffBragg.sim_data import SimData
     import numpy as np
@@ -23,15 +74,10 @@ if __name__ == "__main__":
     from copy import deepcopy
     from dxtbx.model.experiment_list import Experiment, ExperimentList, ExperimentListFactory
 
-    parser = ArgumentParser()
-    parser.add_argument("--plot", action='store_true')
-    parser.add_argument("--scaleonly", action='store_true')
-    args = parser.parse_args()
-
-
     # Initial r0222 regression
     timestamps_of_interest = ['20180501143533988', # bad
                               '20180501143701853'] # Does not work
+
 
     ts = timestamps_of_interest[0]
     data = process_ls49_image_real(tstamp=ts,Nstrongest=10, resmin=2.0, resmax=13.5)
@@ -52,8 +98,6 @@ if __name__ == "__main__":
     # Some global variables here for LS49
     mos_spread_deg=0.01
     n_mos_domains=1
-    
-
     a,b,c,_,_,_ = C.get_unit_cell().parameters()
     Deff = 1000
     Ncells_abc = np.power(4/3*np.pi*Deff**3/a/b/c, 1/3.)
@@ -82,25 +126,6 @@ if __name__ == "__main__":
 
     ucell = C.get_unit_cell().parameters()
     UcellMan = MonoclinicManager(a=ucell[0], b=ucell[1], c=ucell[2], beta=ucell[4]*np.pi / 180.)
-    import pylab as plt
-    if args.plot:
-        I = data["data_img"].copy()
-        M = ~data["mask"]
-        I*=M
-        m = I[I > 0].mean()
-        s = I[I > 0].std()
-        vmin = m-s
-        vmax=m+2.5*s
-
-        plt.imshow(I, vmin=vmin, vmax=vmax)
-        for x1, x2, y1, y2 in data["bboxes_x1x2y1y2"]:
-            patch = plt.Rectangle(
-                width=x2-x1,
-                height=y2-y1,
-                xy=(x1, y1),
-                ec='r', fc='none')
-            plt.gca().add_patch(patch)
-        plt.show()
 
     RUC = RefineAll(
         spot_rois=data["bboxes_x1x2y1y2"],
@@ -142,23 +167,6 @@ if __name__ == "__main__":
     refined_ncells = RUC.x[7]
     refined_scale = RUC.x[10]
 
-    if False:
-      for i_spot in range(RUC.n_spots):
-        fig, axs = plt.subplots(1,2)
-        axs[0].imshow([[0, 1, 1], [0, 1, 2]])
-        axs[1].imshow([[0, 1, 1], [0, 1, 2]])
-        x = RUC.store_model_Lambda[i_spot]
-        y = RUC.store_Imeas[i_spot]
-        vmin = RUC.store_vmin[i_spot]
-        vmax = RUC.store_vmax[i_spot]
-        axs[0].images[0].set_data(x)
-        axs[1].images[0].set_data(y)
-        axs[0].images[0].set_clim(vmin, vmax)
-        axs[1].images[0].set_clim(vmin, vmax)
-        plt.suptitle("Spot number = %d"%i_spot)
-      plt.show()
-
-    #from IPython import embed; embed(); exit() 
     # Now set the other stuff to refine
     print (' ================== now refining all the variables ======================= ')
     Ncells_abc = refined_ncells #np.power(4/3*np.pi*Deff**3/a/b/c, 1/3.)
@@ -211,7 +219,6 @@ if __name__ == "__main__":
     RUC2.use_curvatures=False #
     RUC2.calc_curvatures=True # if set to False, never uses curvatures
     RUC2.run()
-    #from IPython import embed; embed(); exit()
 
     print("Done.")
     print("Refined scale =%f", RUC.x[-1])
@@ -240,3 +247,9 @@ if __name__ == "__main__":
     dump_explist = ExperimentList([exp])
     dump_explist.as_file('after_refinement_%s.expt'%ts)
 
+if __name__ == "__main__":
+  try:
+    script = Script()
+    script.run()
+  except Exception as e:
+    print (str(e))
